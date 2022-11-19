c6225e242a5c5fd75936c9b673cfdcc95c407129
mohanad <Abadrri@gmx.net>
2022-11-19 18:08:29 +0100

    if - else-if switch case break;

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
+// *Author : Mohanad AlBadri
+// * Date: 18/11/2022
+// * recover java skills
+
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Enter your number to No1");
+        Scanner sc = new Scanner(System.in);
+        int number_one = sc.nextInt();
+        System.out.println("Enter your number to NO2");
 
+        double number_tow = sc.nextDouble();
+        Scanner x=new Scanner(System.in);
+        System.out.println("inter your choice");
+        int input_choice;
+int choice = x.nextInt();
+        switch (choice) {
+            case 1:
 
+                System.out.println(number_one+number_tow);
+                break;
 
-        System.out.println("Hello world"+"\t"+ "iam fine");
+            case 2:
+
+                System.out.println( number_one-number_tow);
+                break;
+            default:
+
+                System.out.println("bye");
+        }
     }
 }
